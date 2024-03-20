62671f304ceec59a80d9a1ff95c881147d981330
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-20 16:00:47 +0100

    feat: add functioning timer

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
+//DOM elements
 const btnStart = document.querySelector("#start-btn");
 const btnStop = document.querySelector("#stop-btn");
 const pomodoro = document.querySelector("#pomodoro");
 
+let roundCounter = 0;
+
 btnStart.addEventListener("click", function(){
-    const timerMinutes = 1
-    let counter = 0;
-    let ratio = 360 / (1000 * 60 * timerMinutes);
+    pomodoroTimer(0.5, 0.2);
+});
+
+
+//FUNCTIONS
+
+//main function, based on two int (the "working time" and the pause, both in minutes)
+function pomodoroTimer(timer, pause){
+    // some delay, allowing the full animation on btn
+    setTimeout(() => btnStart.setAttribute("disabled", "disabled"), 500)
+    //sounds
+    const pauseStart = new Audio('sound/ding.mp3');
+    const timerStart = new Audio('sound/ping.mp3');
+    const timerEnd = new Audio('sound/noice.mp3');
+    //variables 
+    const timerMilliSec = minuteToMilliSec(timer);
+    const pauseMilliSec = minuteToMilliSec(pause);
+    const rounds = 2
+    let counter = 1000;
+    let ratio = 360 / timerMilliSec;
+
+    //main part
     let pomodoroRotation = setInterval(function(){
-        console.log(counter);
-        counter += 1000;
         let rotation = counter * ratio;
         pomodoro.style.transform = `rotate(${rotation}deg)`;
-        console.log(pomodoro)
-        if (counter === 3000){
+        counter += 1000;
+        if (rotation === 360 && roundCounter >= rounds){
+            timerEnd.play();
+            clearInterval(pomodoroRotation);
+            btnStart.removeAttribute("disabled");
+            counter = 1000;
+            roundCounter = 0;
+            pomodoro.removeAttribute("style");
+        } else if (rotation === 360){
+            pauseStart.play();
             clearInterval(pomodoroRotation)
+            counter = 1000;
+            pomodoro.style.filter = "hue-rotate(90deg)"
+            setTimeout(function(){
+                pomodoro.removeAttribute("style")
+                timerStart.play();
+            }, pauseMilliSec)
         }
+        btnStop.addEventListener("click", function(){
+            clearInterval(pomodoroRotation);
+            btnStart.removeAttribute("disabled");
+            counter = 1000;
+            roundCounter = 0;
+            pomodoro.removeAttribute("style");
+        })
     }, 1000);
-})
+    roundCounter++;
+    if (roundCounter < rounds) {
+        setTimeout(pomodoroTimer, (timerMilliSec + pauseMilliSec), timer, pause)
+    }
+}
 
+//lil function converting minutes in millisecs
+function minuteToMilliSec(number){
+    return (1000 * 60 * number)
+}
