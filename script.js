22d8d1ba94f2c8781655c943d892ec87398b8e1e
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-20 18:36:29 +0100

    feat: add tick sound to working time + 10millisec buffer between every set of rounds

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ btnStart.addEventListener("click", function(){
 //FUNCTIONS
 
 //main function, based on two int (the "working time" and the pause, both in minutes)
-function pomodoroTimer(timer, pause){
+function pomodoroTimer(timer, pause){    
     // some delay, allowing the full animation on btn
     setTimeout(() => btnStart.setAttribute("disabled", "disabled"), 500);
     //sounds
     const pauseStart = new Audio('sound/ding.mp3');
     const timerStart = new Audio('sound/ping.mp3');
     const timerEnd = new Audio('sound/noice.mp3');
+    const tick = new Audio('sound/tick.mp3');
     //variables
     const timerMilliSec = minuteToMilliSec(timer);
     const pauseMilliSec = minuteToMilliSec(pause);
@@ -29,31 +30,33 @@ function pomodoroTimer(timer, pause){
 
     //main part
     let pomodoroRotation = setInterval(function(){
-        let rotation = counter * ratio;
+        let rotation = counter * ratio;        
+        tick.play();
         pomodoro.style.transform = `rotate(${rotation}deg)`;
-        counter += 1000;
+        counter += 1000;        
         if (rotation === 360 && roundCounter >= rounds){
             timerEnd.play();
             clearInterval(pomodoroRotation);
             btnStart.removeAttribute("disabled");
             counter = 1000;
             roundCounter = 0;
-            pomodoro.removeAttribute("style");
-        } else if (rotation === 360){
-            pauseStart.play();
-            clearInterval(pomodoroRotation)
+            pomodoro.removeAttribute("style");            
+        } else if (rotation === 360){            
+            pauseStart.play();            
+            clearInterval(pomodoroRotation)            
             counter = 1000;
             pomodoro.style.filter = "hue-rotate(90deg)"
             setTimeout(function(){
                 pomodoro.removeAttribute("style");
-                timerStart.play();
+                timerStart.play();                
             }, pauseMilliSec)
         }
     }, 1000);
 
     roundCounter++;
     if (roundCounter < rounds) {
-        setTimeout(pomodoroTimer, (timerMilliSec + pauseMilliSec), timer, pause);
+        //10 seconds buffer so it effectively ends the previous cicle before starting this one
+        setTimeout(pomodoroTimer, (timerMilliSec + pauseMilliSec + 10), timer, pause);
     }
 }
 
