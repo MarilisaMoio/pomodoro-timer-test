527af6836271a22a67896f85cd8a8086daf04c67
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-26 12:24:35 +0100

    fix: change the way it's recognize working time's end on each round

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const btnStop = document.querySelector("#stop-btn");
 const pomodoro = document.querySelector("#pomodoro");
 
 let roundCounter = 0;
+let counter = 1000;
+
+let pomodoroRotation;
+let setRepetition;
+let setEndPause;
 
 btnStart.addEventListener("click", function(){
     //inputs
     const workingTime = parseInt(document.querySelector("#working-time").value)
     const pauseTime = parseInt(document.querySelector("#pause").value)
     const howManyRounds = parseInt(document.querySelector("#rounds").value)
+    
+    //some delay, allowing the full animation on btn
+    setTimeout(() => btnStart.setAttribute("disabled", "disabled"), 500);
+
+    //debug values
+    // const workingTime = 10;
+    // const pauseTime = 5;
+    // const howManyRounds = 4;
 
 
     if(!isNaN(workingTime) && !isNaN(pauseTime) && !isNaN(howManyRounds)){
@@ -17,13 +30,10 @@ btnStart.addEventListener("click", function(){
     }
 });
 
-
 //FUNCTIONS
 
 //main function, based on two int (the "working time" and the pause, both in minutes)
-function pomodoroTimer(timer, pause, rounds){    
-    // some delay, allowing the full animation on btn
-    setTimeout(() => btnStart.setAttribute("disabled", "disabled"), 500);
+function pomodoroTimer(timer, pause, rounds){
     //sounds
     const pauseStart = new Audio('sound/ding.mp3');
     const timerStart = new Audio('sound/ping.mp3');
@@ -32,38 +42,37 @@ function pomodoroTimer(timer, pause, rounds){
     //variables
     const timerMilliSec = minuteToMilliSec(timer);
     const pauseMilliSec = minuteToMilliSec(pause);
-    let counter = 1000;
     let ratio = 360 / timerMilliSec;
 
     //main part
-    let pomodoroRotation = setInterval(function(){
-        let rotation = counter * ratio;        
+    pomodoroRotation = setInterval(function(){
+        let rotation = counter * ratio;
         tick.play();
         pomodoro.style.transform = `rotate(${rotation}deg)`;
-        counter += 1000;        
-        if (rotation === 360 && roundCounter >= rounds){
+        counter += 1000;
+        if (counter === timerMilliSec + 1000 && roundCounter >= rounds){
+            (`senza pausa ${roundCounter - 1}`)
             timerEnd.play();
             clearInterval(pomodoroRotation);
             btnStart.removeAttribute("disabled");
             counter = 1000;
             roundCounter = 0;
-            pomodoro.removeAttribute("style");            
-        } else if (rotation === 360){            
-            pauseStart.play();            
-            clearInterval(pomodoroRotation)            
+            pomodoro.removeAttribute("style");
+        } else if (counter === timerMilliSec + 1000){
+            setEndPause = setTimeout(function(){
+                pomodoro.removeAttribute("style");
+                timerStart.play();
+            }, pauseMilliSec - 10) //10ms to counter the variable execution time 
+            pauseStart.play();
+            clearInterval(pomodoroRotation)
             counter = 1000;
             pomodoro.style.filter = "hue-rotate(90deg)"
-            setTimeout(function(){
-                pomodoro.removeAttribute("style");
-                timerStart.play();                
-            }, pauseMilliSec)
         }
     }, 1000);
 
     roundCounter++;
     if (roundCounter < rounds) {
-        //10 seconds buffer so it effectively ends the previous cicle before starting this one
-        setTimeout(pomodoroTimer, (timerMilliSec + pauseMilliSec + 10), timer, pause, rounds);
+        setRepetition = setTimeout(pomodoroTimer, (timerMilliSec + pauseMilliSec), timer, pause, rounds);
     }
 }
 
